c10a0839199913ed81a0a900b1f743e5ebaa6d68
TropicolCoder <jacobsbusayo@gmail.com>
2021-05-15 20:27:25 +0100

    Responsive and aos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    500: {
+        items: 1
+    },
+    768: {
+        items: 2
+    },
+    960: {
+        items: 3
+    },
+}
+
 $(document).ready(function() {
 
     $nav = $('.nav');
@@ -15,8 +30,19 @@ $(document).ready(function() {
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive,
+
     });
 
+    // click to scroll top
+    $('.move-up span').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
 
 });
